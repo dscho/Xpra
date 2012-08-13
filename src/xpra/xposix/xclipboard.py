6453018a5af8dde5b43101518c0f1ef5b7d8c898
antoine <antoine@3bb7dfac-3a0b-4e04-842a-767bc560f471>
2012-08-13 11:42:47 +0000

    #176: clipboard fixes from r1314, r1315, r1317 and r1318 backported to 0.3.x and 0.4.x branches
    git-svn-id: https://www.xpra.org/svn/Xpra/tags/v0.4.x@1320 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/xposix/xclipboard.py
+++ src/xpra/xposix/xclipboard.py
@@ -7,10 +7,8 @@ import struct
 
 from wimpiggy.lowlevel import (gdk_atom_objects_from_gdk_atom_array, #@UnresolvedImport
                                gdk_atom_array_from_gdk_atom_objects) #@UnresolvedImport
-from wimpiggy.log import Logger
-log = Logger()
 
-from xpra.platform.clipboard_base import ClipboardProtocolHelperBase
+from xpra.platform.clipboard_base import ClipboardProtocolHelperBase, debug
 
 class ClipboardProtocolHelper(ClipboardProtocolHelperBase):
     """ This clipboard helper adds the ability to parse raw X11 atoms
@@ -27,16 +25,24 @@ class ClipboardProtocolHelper(ClipboardProtocolHelperBase):
             # as a byte buffer.
             atoms = gdk_atom_objects_from_gdk_atom_array(data)
             atom_names = [str(atom) for atom in atoms]
-            if target=="TARGET":
-                atom_names.remove("SAVE_TARGETS")
-                atom_names.remove("COMPOUND_TEXT")
+            if target=="TARGETS":
+                otargets = list(atom_names)
+                discard_targets = ("SAVE_TARGETS", "COMPOUND_TEXT")
+                for x in discard_targets:
+                    if x in atom_names:
+                        atom_names.remove(x)
+                debug("_do_munge_raw_selection_to_wire(%s, %s, %s, %s:%s) filtered targets(%s)=%s", target, dtype, dformat, type(data), len(data), otargets, atom_names)
             return ("atoms", atom_names)
         return ClipboardProtocolHelperBase._do_munge_raw_selection_to_wire(self, target, dtype, dformat, data)
 
     def _munge_wire_selection_to_raw(self, encoding, dtype, dformat, data):
+        debug("_munge_wire_selection_to_raw(%s, %s, %s, %s:%s:%s)", encoding, dtype, dformat, type(data), len(data or ""), list(data or ""))
         if encoding == "atoms":
             import gtk.gdk
             gdk_atoms = [gtk.gdk.atom_intern(a) for a in data]
             atom_array = gdk_atom_array_from_gdk_atom_objects(gdk_atoms)
-            return struct.pack("=" + "L" * len(atom_array), *atom_array)
+            bdata = struct.pack("=" + "Q" * len(atom_array), *atom_array)
+            debug("_munge_wire_selection_to_raw(%s, %s, %s, %s:%s)=%s=%s=%s", encoding, dtype, dformat, type(data), len(data or ""), gdk_atoms, atom_array, list(bdata))
+            return bdata
         return ClipboardProtocolHelperBase._munge_wire_selection_to_raw(self, encoding, dtype, dformat, data)
+
