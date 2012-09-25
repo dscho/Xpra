8a152be297699ef022ff93ec32d790df81bc8565
antoine <antoine@3bb7dfac-3a0b-4e04-842a-767bc560f471>
2012-09-25 16:21:21 +0000

    do not copy the last_delays variable or all the windows will end up sharing it!
    git-svn-id: https://www.xpra.org/svn/Xpra/tags/v0.6.x@1689 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/window_source.py
+++ src/xpra/window_source.py
@@ -76,7 +76,7 @@ class DamageBatchConfig(object):
     def clone(self):
         c = DamageBatchConfig()
         for x in ["enabled", "always", "max_events", "max_pixels", "time_unit",
-                  "min_delay", "max_delay", "delay", "last_delays"]:
+                  "min_delay", "max_delay", "delay"]:
             setattr(c, x, getattr(self, x))
         return c
 
