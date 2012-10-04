c53434824caaa71cd967bdb7000d670c5e044934
antoine <antoine@3bb7dfac-3a0b-4e04-842a-767bc560f471>
2012-10-04 13:54:19 +0000

    r1789 (partial): take shortcut if damage area is empty (zero size)
    git-svn-id: https://www.xpra.org/svn/Xpra@1800 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- tags/v0.3.x/src/xpra/server.py
+++ tags/v0.3.x/src/xpra/server.py
@@ -379,6 +379,8 @@ class ServerSource(object):
             the options may get quashed! So, specify a "batching"=False
             option to ensure no batching will occur for this request.
         """
+        if w==0 or h==0:
+            return
         batch = self.get_batch_config(wid)
         def damage_now(reason):
             self._sequence += 1
@@ -559,6 +561,8 @@ class ServerSource(object):
             time.sleep(0)
 
     def _process_damage_regions(self, wid, window, ww, wh, regions, coding, sequence, options):
+        if w==0 or h==0:
+            return
         if self._damage_cancelled.get(wid, 0)>=sequence:
             log("process_damage_regions: dropping damage request with sequence=%s", sequence)
             return
