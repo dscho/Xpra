c1c91b529d9bd4d3cca990218968872a93d0ea34
antoine <antoine@3bb7dfac-3a0b-4e04-842a-767bc560f471>
2012-10-12 17:57:31 +0000

    0.3.12 release
    git-svn-id: https://www.xpra.org/svn/Xpra/tags/v0.3.x@1975 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/__init__.py
+++ src/xpra/__init__.py
@@ -3,6 +3,6 @@
 # Parti is released under the terms of the GNU GPL v2, or, at your option, any
 # later version. See the file COPYING for details.
 
-__version__ = "0.3.11"
+__version__ = "0.3.12"
 import wimpiggy
 assert wimpiggy.__version__ == __version__
