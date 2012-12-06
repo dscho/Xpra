70622167f6e77d071a3e582dd6c1007a1778a791
Johannes Schindelin <johannes.schindelin@gmx.de>
2012-12-06 12:06:04 -0600

    'start.sh attach ...' automatically adds --no-keyboard-sync now
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- start.sh
+++ start.sh
@@ -20,6 +20,10 @@ export PYTHONPATH=$PWD/src/install/lib/python:$PYTHONPATH
 
 if test $# -gt 0
 then
+	test "attach" = "$1" && {
+		shift
+		set attach --no-keyboard-sync "$@"
+	}
 	exec ./src/install/bin/xpra "$@"
 else
 	DISPLAY=98
