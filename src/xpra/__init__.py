88584d03ab8f1ac48156c4d887cf8e5dcd7dffbd
antoine <antoine@3bb7dfac-3a0b-4e04-842a-767bc560f471>
2013-01-03 05:27:13 +0000

    bump to 0.7.7 and add release notes
    git-svn-id: https://www.xpra.org/svn/Xpra/tags/v0.7.x@2432 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/__init__.py
+++ src/xpra/__init__.py
@@ -3,6 +3,6 @@
 # Parti is released under the terms of the GNU GPL v2, or, at your option, any
 # later version. See the file COPYING for details.
 
-__version__ = "0.7.6"
+__version__ = "0.7.7"
 import wimpiggy
 assert wimpiggy.__version__ == __version__
