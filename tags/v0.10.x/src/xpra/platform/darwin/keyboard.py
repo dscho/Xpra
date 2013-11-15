f9c646c6bdd17a98db85871dad7ab733b2287b38
antoine <antoine@3bb7dfac-3a0b-4e04-842a-767bc560f471>
2013-11-15 04:05:31 +0000

    #453: apply part of r4747 to v0.10.x branch: enable numlock by default on osx and allow toggle
    git-svn-id: https://www.xpra.org/svn/Xpra@4750 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- tags/v0.10.x/src/xpra/platform/darwin/keyboard.py
+++ tags/v0.10.x/src/xpra/platform/darwin/keyboard.py
@@ -9,8 +9,33 @@ from xpra.platform.keyboard_base import KeyboardBase
 from xpra.log import Logger
 log = Logger()
 
+NUM_LOCK_KEYCODE = 71           #HARDCODED! 
+
 
 class Keyboard(KeyboardBase):
 
     def __init__(self):
-        pass
+        self.num_lock_modifier = None
+        self.num_lock_state = True
+        self.num_lock_keycode = NUM_LOCK_KEYCODE
+
+    def set_modifier_mappings(self, mappings):
+        KeyboardBase.set_modifier_mappings(self, mappings)
+        self.num_lock_modifier = self.modifier_keys.get("Num_Lock")
+
+    def mask_to_names(self, mask):
+        names = KeyboardBase.mask_to_names(self, mask)
+        if self.num_lock_modifier is not None:
+            if self.num_lock_state and self.num_lock_modifier not in names:
+                names.append(self.num_lock_modifier)
+            elif not self.num_lock_state and self.num_lock_modifier in names:
+                names.remove(self.num_lock_modifier)
+        log("mask_to_names(%s)=%s", mask, names)
+        return names
+
+    def process_key_event(self, send_key_action_cb, wid, key_event):
+        if key_event.keycode==self.num_lock_keycode and not key_event.pressed:
+            log("toggling numlock")
+            self.num_lock_state = not self.num_lock_state
+        send_key_action_cb(wid, key_event)
+
