e68dd9727fe60fd0a79890e7fae214f6859f6c1d
antoine <antoine@3bb7dfac-3a0b-4e04-842a-767bc560f471>
2013-11-17 07:51:24 +0000

    r4719 for v0.10.x branch: automatically scale very large windows so video encoders won't choke on them
    git-svn-id: https://www.xpra.org/svn/Xpra@4766 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- tags/v0.10.x/src/xpra/server/window_video_source.py
+++ tags/v0.10.x/src/xpra/server/window_video_source.py
@@ -379,7 +379,13 @@ class WindowVideoSource(WindowSource):
             #no scaling window attribute defined, so use heuristics to enable:
             quality = self.get_current_quality()
             speed = self.get_current_speed()
-            if width*height>=1024*1024 and quality<30 and speed>90:
+            if width>=4096 or height>=4096:
+                #most encoders can't deal with that!
+                d = 1
+                while width/d>=4096 or height/d>=4096:
+                    d += 1
+                self.actual_scaling = 1,d
+            elif width*height>=1024*1024 and quality<30 and speed>90:
                 self.actual_scaling = 2,3
             elif self.maximized and quality<50 and speed>80:
                 self.actual_scaling = 2,3
