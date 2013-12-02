a680bacc070904e1700447a5c0b4054a8d07b4de
antoine <antoine@3bb7dfac-3a0b-4e04-842a-767bc560f471>
2013-12-02 16:15:00 +0000

    the new dec_avcodec2 uses AtomicInteger, so we must backport that too... (endless!)
    git-svn-id: https://www.xpra.org/svn/Xpra/tags/v0.10.x@4837 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- src/xpra/util.py
+++ src/xpra/util.py
@@ -17,6 +17,32 @@ class AdHocStruct(object):
         return ("<%s object, contents: %r>"
                 % (type(self).__name__, self.__dict__))
 
+
+class AtomicInteger(object):
+    def __init__(self, integer = 0):
+        self.counter = integer
+        self.lock = threading.RLock()
+
+    def increase(self, inc = 1):
+        self.lock.acquire()
+        self.counter = self.counter + inc
+        v = self.counter
+        self.lock.release()
+        return v
+
+    def decrease(self, dec = 1):
+        self.lock.acquire()
+        self.counter = self.counter - dec
+        v = self.counter
+        self.lock.release()
+        return v
+
+    def get(self):
+        return self.counter
+
+    def __str__(self):
+        return str(self.counter)
+
 def alnum(_str):
     return filter(str.isalnum, _str)
 
